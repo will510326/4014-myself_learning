66b4ecf72deef1864b10dff2af500dbe98ea7969
will510326 <will510326@gmail.com>
2025-12-30 14:14:54 +0800

    format tf file

--- Hands-On_ALB/main.tf
+++ Hands-On_ALB/main.tf
@@ -10,17 +10,17 @@ terraform {
 
 # 2. 設定區域
 provider "aws" {
-  region = "ap-northeast-1" 
+  region = "ap-northeast-1"
 }
 
 # 自動去找最新版的ubuntu
 data "aws_ami" "ubuntu" {
   most_recent = true
-  owners = ["099720109477"]
+  owners      = ["099720109477"]
 
   filter {
-    name = "name"
-    values = [ "ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*" ]
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
 }
 
@@ -29,34 +29,34 @@ data "aws_vpc" "default_vpc" {
   default = true
 }
 
-data "aws_subnet" "default" {
+data "aws_subnets" "default" {
   filter {
-    name = "vpc-id"
-    values = [ data.aws_vpc.default_vpc.id ]
+    name   = "vpc-id"
+    values = [data.aws_vpc.default_vpc.id]
   }
 }
 
 
 resource "aws_security_group" "my_sg" {
-  name = "my-sg"
+  name   = "my-sg"
   vpc_id = data.aws_vpc.default_vpc.id
 
   ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
   tags = {
@@ -66,31 +66,31 @@ resource "aws_security_group" "my_sg" {
 
 # create ALB security group
 resource "aws_security_group" "alb_sg" {
-  name = "wayne-alb-sg"
+  name        = "wayne-alb-sg"
   description = "Allow HTTP traffic to ALB"
-  vpc_id = data.aws_vpc.default_vpc.id
+  vpc_id      = data.aws_vpc.default_vpc.id
 
   ingress {
     description = "HTTP from Internet."
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = [ "0.0.0.0/0" ]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = [ "0.0.0.0/0" ]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-resource "aws_alb" "wayne_alb" {
-  name = "wayne-web-alb"
-  internal = false # 對外開放
+resource "aws_lb" "wayne_alb" {
+  name               = "wayne-web-alb"
+  internal           = false # 對外開放
   load_balancer_type = "application"
-  security_groups = [ aws_security_group.alb_sg.id ]
-  subnets = data.aws_ami.ubuntu.default.ids # Fetch subnets
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = data.aws_subnets.default.ids # Fetch subnets
 
   tags = {
     Name = "Wayne-ALB"
@@ -99,32 +99,32 @@ resource "aws_alb" "wayne_alb" {
 
 # target group
 resource "aws_lb_target_group" "wayne-tg" {
-  name = "wayne-web-tg"
-  port = 80
+  name     = "wayne-web-tg"
+  port     = 80
   protocol = "HTTP"
-  vpc_id = data.aws_vpc.default_vpc.id
+  vpc_id   = data.aws_vpc.default_vpc.id
 
   # Health Check
   # ALB will be ping this path to confirm EC2 status.
   health_check {
-    path = "/"
-    protocol = "HTTP"
-    matcher = "200"
-    interval = 30
-    timeout = 5
-    healthy_threshold = 2
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
     unhealthy_threshold = 2
   }
 }
 
 # Listener - This responsible is transfer internal to target group
 resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_alb.wayne_alb.arn # 掛在哪一個ALB上
-  port = 80
-  protocol = "HTTP"
+  load_balancer_arn = aws_lb.wayne_alb.arn # 掛在哪一個ALB上
+  port              = 80
+  protocol          = "HTTP"
 
   default_action {
-    type = "forward"
+    type             = "forward"
     target_group_arn = aws_lb_target_group.wayne-tg.arn # 轉發給哪一個TG
   }
 }
@@ -132,24 +132,24 @@ resource "aws_lb_listener" "http" {
 # 把你的EC2註冊到Target Group內
 resource "aws_lb_target_group_attachment" "web_attachment" {
   target_group_arn = aws_lb_target_group.wayne-tg.arn
-  target_id = aws_instance.web.id # 引用原本的EC2 ID
-  port = 80
+  target_id        = aws_instance.web.id # 引用原本的EC2 ID
+  port             = 80
 }
 
 # key
 resource "aws_key_pair" "waynelocalkey" {
-  key_name = "wayne-key-iac"
+  key_name   = "wayne-key-iac"
   public_key = file("/home/wayne/.ssh/id_ed25519.pub")
 }
 
 resource "aws_instance" "web" {
-  ami = data.aws_ami.ubuntu.id
+  ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
 
   # key
   key_name = aws_key_pair.waynelocalkey.key_name
 
-  vpc_security_group_ids = [ aws_security_group.my_sg.id ]
+  vpc_security_group_ids = [aws_security_group.my_sg.id]
 
   # execute script
   user_data = file("${path.module}/user-data.sh")
@@ -157,7 +157,7 @@ resource "aws_instance" "web" {
   user_data_replace_on_change = true
 
   tags = {
-    Name = "Wayne-Iac-Server"
+    Name    = "Wayne-Iac-Server"
     Project = "SRE-Learning"
   }
 }
@@ -165,12 +165,12 @@ resource "aws_instance" "web" {
 #output
 output "server_public_ip" {
   description = "Public IP address of the EC2 instance"
-  value = aws_instance.web.public_ip
+  value       = aws_instance.web.public_ip
 }
 
 output "server_ssh_command" {
   description = "Command to SSH into the instance"
-  value = "ssh -i /home/wayne/.ssh/id_ed25519 ubuntu@${aws_instance.web.public_ip}"
+  value       = "ssh -i /home/wayne/.ssh/id_ed25519 ubuntu@${aws_instance.web.public_ip}"
 }
 
 output "alb_dns_name" {
